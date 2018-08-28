6fffca18d4eeef84ba0f7688a8794b10742146a9
BordenJardine <bordenjardine@gmail.com>
2018-08-28 18:54:08 -0400

    Create skull.sh

---
+++ skull.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+RED=$(tput setaf 1)
+WHITE=$(tput setaf 7)
+
+IFS='' read -r -d '' SKULL <<'EOF'
+                        ...----....
+                    ..-:"''         ''"-..
+                 .-'                      '-.
+               .'              .     .       '.
+             .'   .          .    .      .    .''.
+           .'  .    .       .   .   .     .   . ..:.
+         .' .   . .  .       .   .   ..  .   . ....::.
+        ..   .   .      .  .    .     .  ..  . ....:IA.
+       .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+      .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+      '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+     .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+    .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+    ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+   .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+  .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+  :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+  ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+  ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+  :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+  '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+   :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+  .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+  ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+: .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+:.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+  "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+   :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+   :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+   :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+   :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+   :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+   ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+     V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+       'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+        I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+       .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+       :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+       :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+       ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+       ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+       :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+       ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+         "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+          ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+            "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+              "::.::.. .. .  ...:::IIHHMMMMHMV"
+                "V::... . .I::IHHMMV"'
+                  '"VHVHHHAHHHHMMV:"'
+EOF
+
+IFS='' read -r -d '' SKULL2 <<EOF
+                        ...----....
+                    ..-:"''         ''"-..
+                 .-'                      '-.
+               .'              .     .       '.
+             .'   .          .    .      .    .''.
+           .'  .    .       .   .   .     .   . ..:.
+         .' .   . .  .       .   .   ..  .   . ....::.
+        ..   .   .      .  .    .     .  ..  . ....:IA.
+       .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+      .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+      '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+     .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+    .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+    ':.:..  ..::${RED}IHHHHHI${WHITE}::. . .  ...:.::::.,,,. . ....VIMMHM
+   .:::I. .${RED}AHHHHHHHHHHAI${WHITE}::. .:...,:${RED}IIHHHHHHMMMHHL${WHITE}:. . VMMMM
+  .:.:V.:${RED}IVHHHHHHHMHMHHH${WHITE}::..:" .:${RED}HIHHHHHHHHHHHHHMHHA${WHITE}. .VMMM.
+  :..V.:${RED}IVHHHHHMMHHHHHHHB${WHITE}... . .:${RED}VPHHMHHHMMHHHHHHHHHAI${WHITE}.:VMMI
+  ::V..:${RED}VIHHHHHHMMMHHHHHH${WHITE}. .   .I":${RED}IIMHHMMHHHHHHHHHHHAPI${WHITE}:WMM
+  ::". .:.${RED}HHHHHHHHMMHHHHHI${WHITE}.  . .:..${RED}I:MHMMHHHHHHHHHMHV${WHITE}:':H:WM
+  :: . :.::${RED}IIHHHHHHMMHHHH${WHITE}V  .ABA.:.:${RED}IMHMHMMMHMHHHHV${WHITE}:'. .IHWW
+  '.  ..:..:.:${RED}IHHHHHMMH${WHITE}V" .AVMHMA.:.'${RED}VHMMMMHHHHHV${WHITE}:' .  :IHWV
+   :.  .:...:".:.:${RED}TPP${WHITE}"   .AVMMHMMA.:. "${RED}VMMHHHP${WHITE}.:... .. :IVAI
+  .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+  ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+: .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+:.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+  "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+   :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+   :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+   :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+   :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+   :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+   ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+     V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+       'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+        I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+       .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+       :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+       :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+       ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+       ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+       :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+       ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+         "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+          ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+            "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+              "::.::.. .. .  ...:::IIHHMMMMHMV"
+                "V::... . .I::IHHMMV"'
+                  '"VHVHHHAHHHHMMV:"'
+EOF
+
+chars_x=31
+chars_y=25
+display_centered() {
+  columns="$(tput cols)"
+  lines="$(tput lines)"
+  spaces=$(( ($columns / 2) - $chars_x ))
+  blank_lines=$(( ($lines / 2) - $chars_y ))
+
+  yes "" | head -n $blank_lines
+
+  while IFS= read -r line; do
+    printf "%*s%s\n" $spaces ' ' "$line"
+  done <<< "$1"
+}
+
+while :; do
+  clear
+
+  if [ $RANDOM -gt 30000 ]; then
+    display_centered "$SKULL2"
+  else
+    display_centered "$SKULL"
+  fi
+  sleep 0.2
+done
